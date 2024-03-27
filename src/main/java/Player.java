81dcf3917f540d32f8ecfe5cfbfb023ecc959b01
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-03-27 13:22:45 +0530

    player calss is used to assign cards to player

---
+++ src/main/java/Player.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private List<Card> hand;
+
+    public Player() {
+        hand = new ArrayList<>();
+    }
+
+    public void selectCard(Card card) {
+        hand.add(card);
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+}
